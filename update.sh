b8e290fc2791701e52ee6fcd9b2432f697a72535
RouxRC <b.ooghe@gmail.com>
2015-09-06 23:52:31 +0200

    make public branch

---
+++ update.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+source /usr/local/bin/virtualenvwrapper.sh
+workon gala2015
+
+python build_index.py > /tmp/update_gala.log 2>&1 ||
+  (cat /tmp/update_gala.log  && exit 1)
+
+if git diff index.html > /dev/null; then
+  git commit index.html
+  git push origin gh-pages
+fi
+
